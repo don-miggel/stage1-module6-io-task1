38a75ad204c9a401b885e38396d425155c4f9777
Mykhailo Derecha <don.miggel@yahoo.com>
2023-04-24 20:43:23 +0300

    stage1 module6 io task done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,56 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.IOException;
+
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String str = "";
+        try (java.io.FileReader fin = new java.io.FileReader(file.getPath())){
+            int i;
+
+            do{
+                i = fin.read();
+                if(i != -1) {
+                    str += (char)i;
+                }
+
+            }while ( i != -1);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] values = parseString(str, ':');
+
+        return new Profile(values[0], Integer.parseInt(values[1]), values[2], Long.parseLong(values[3]));
+    }
+
+    private int countSeparator(String str, char sep){
+        int count=0;
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == sep)
+                count++;
+       }
+        return count;
+    }
+
+    private String[] parseString(String str, char sep){
+        String[] values = new String[countSeparator(str, sep)];
+        int arrCounter = 0;
+        String currentStr = str;
+        while (currentStr.indexOf(sep) != -1){
+            values[arrCounter++] = currentStr.substring(currentStr.indexOf(':')+1, currentStr.indexOf('\n')).trim();
+            currentStr= currentStr.substring(currentStr.indexOf('\n')+1);
+        };
+        return values;
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader();
+        File fl = new File("src/main/java/com/epam/mjc/io/pr.txt");
+        System.out.println(fr.getDataFromFile(fl));
     }
 }
